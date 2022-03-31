7ac67f010ca9b2ce59670bf1bc70392e85335fb2
Ibrohim Fattohov <ibrohim97uz@gmail.com>
2022-03-31 20:13:17 +0500

    a

--- src/hooks/use-query/index.js
+++
@@ -1,8 +0,0 @@
-import { useQuery } from 'react-query';
-import MainRequest from '../main-request';
-
-const useGeneralApi = (url, params, options) => {
-	return useQuery([url, params], async () => MainRequest.get(url, { params }), { ...options });
-};
-
-export default useGeneralApi;
